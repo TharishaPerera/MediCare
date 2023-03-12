3ce194a46fce0710dcafca9124c5ebd7b8344b2d
Tharisha Perera <tharisha.perera@cammsgroup.com>
2023-03-12 12:05:32 +0530

    improved verification page

--- app/src/main/java/com/nibm/medicare/VerificationActivity.kt
+++ app/src/main/java/com/nibm/medicare/VerificationActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 
 class VerificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,11 +32,15 @@ class VerificationActivity : AppCompatActivity() {
         digi3 = findViewById(R.id.txt_digi_3)
         digi4 = findViewById(R.id.txt_digi_4)
 
+        changeFocus(digi1, digi2, digi3, digi4)
+
         imgClear.setOnClickListener{
             digi1.text = null
             digi2.text = null
             digi3.text = null
             digi4.text = null
+
+            enableDigi1(digi1, digi2, digi3, digi4)
         }
 
         txtChangeNo.setOnClickListener{
@@ -49,4 +54,35 @@ class VerificationActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun changeFocus(digi1: EditText, digi2: EditText, digi3: EditText, digi4: EditText) {
+        enableDigi1(digi1, digi2, digi3, digi4)
+
+        digi1.addTextChangedListener {
+            if (digi1.text.isNotEmpty()){
+                digi2.isEnabled = true
+                digi2.requestFocus()
+            }
+        }
+        digi2.addTextChangedListener {
+            if (digi2.text.isNotEmpty()){
+                digi3.isEnabled = true
+                digi3.requestFocus()
+            }
+        }
+        digi3.addTextChangedListener {
+            if (digi3.text.isNotEmpty()){
+                digi4.isEnabled = true
+                digi4.requestFocus()
+            }
+        }
+    }
+
+    private fun enableDigi1(digi1: EditText, digi2: EditText, digi3: EditText, digi4: EditText) {
+        digi1.requestFocus()
+        digi2.isEnabled = false
+        digi3.isEnabled = false
+        digi4.isEnabled = false
+    }
+
 }
