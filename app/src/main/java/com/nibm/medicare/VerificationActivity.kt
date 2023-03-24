54a08928cbb1e18811eaade668adeef965bb5597
Ammaar <ammaar.nizam@outlook.com>
2023-03-24 12:30:01 +0530

    Added SMS OTP verification with Firebase

--- app/src/main/java/com/nibm/medicare/VerificationActivity.kt
+++ app/src/main/java/com/nibm/medicare/VerificationActivity.kt
@@ -30,18 +30,18 @@ class VerificationActivity : AppCompatActivity() {
     private lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
     private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
 
-    private var generatedOTP : Int = 0
-    private var mobileNumber : String = ""
+//    private var generatedOTP : Int = 0
+//    private var mobileNumber : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verification)
 
-        val bundle = intent.extras
-        if (bundle != null){
-            generatedOTP = bundle.getInt("OTP")
-            mobileNumber = bundle.getString("mobileNumber").toString()
-        }
+//        val bundle = intent.extras
+//        if (bundle != null){
+//            generatedOTP = bundle.getInt("OTP")
+//            mobileNumber = bundle.getString("mobileNumber").toString()
+//        }
 
         FirebaseApp.initializeApp(this)
 
@@ -89,100 +89,71 @@ class VerificationActivity : AppCompatActivity() {
             }
         }
 
-        var btnContinue : Button
-        var txtChangeNo : TextView
-        var txtResndCode : TextView
-        var imgClear : ImageView
-        var digi1 : EditText
-        var digi2 : EditText
-        var digi3 : EditText
-        var digi4 : EditText
+        var editTextPhone : EditText = findViewById(R.id.editTextPhone)
+        var editTextPhone2 : EditText = findViewById(R.id.editTextPhone2)
+        var sendOTPButton: Button = findViewById(R.id.sendOTPButton)
+        var verifyButton : Button = findViewById(R.id.verifyButton)
 
-        btnContinue = findViewById(R.id.btn_continue)
-        txtChangeNo = findViewById(R.id.txt_change_no)
-        txtResndCode = findViewById(R.id.txt_resend_code)
-        imgClear = findViewById(R.id.img_clear)
-        digi1 = findViewById(R.id.txt_digi_1)
-        digi2 = findViewById(R.id.txt_digi_2)
-        digi3 = findViewById(R.id.txt_digi_3)
-        digi4 = findViewById(R.id.txt_digi_4)
-
-        changeFocus(digi1, digi2, digi3, digi4)
-
-        imgClear.setOnClickListener{
-            clearFields(digi1, digi2, digi3, digi4)
-            enableDigi1(digi1, digi2, digi3, digi4)
+        sendOTPButton.setOnClickListener{
+            startPhoneNumberVerification(editTextPhone!!.text.toString())
         }
 
-        txtResndCode.setOnClickListener {
-            resendCode()
-            clearFields(digi1, digi2, digi3, digi4)
-            enableDigi1(digi1, digi2, digi3, digi4)
-        }
-
-        txtChangeNo.setOnClickListener{
-            var intent = Intent(this, WelcomeActivity::class.java)
-            startActivity(intent)
-        }
-
-        btnContinue.setOnClickListener {
-            startPhoneNumberVerification(mobileNumber)
-            val userOtp = digi1.text.toString() + digi2.text.toString() + digi3.text.toString() + digi4.text.toString()
-            verifyPhoneNumberWithCode(storedVerificationId, userOtp)
+        verifyButton.setOnClickListener {
+            verifyPhoneNumberWithCode(storedVerificationId, editTextPhone2!!.text.toString())
         }
 
     }
 
-    private fun resendCode() {
-        var sms = SMS()
-        val OTP = sms.generateOTP()
-        generatedOTP = OTP
-        try {
-            val queue = Volley.newRequestQueue(this)
-            sms.sendOTP(mobileNumber, OTP, queue)
+//    private fun resendCode() {
+//        var sms = SMS()
+//        val OTP = sms.generateOTP()
+//        generatedOTP = OTP
+//        try {
+//            val queue = Volley.newRequestQueue(this)
+//            sms.sendOTP(mobileNumber, OTP, queue)
+//
+//            Toast.makeText(applicationContext, "OTP Sent", Toast.LENGTH_LONG).show()
+//        } catch (error: Exception) {
+//            println(error.message)
+//        }
+//    }
 
-            Toast.makeText(applicationContext, "OTP Sent", Toast.LENGTH_LONG).show()
-        } catch (error: Exception) {
-            println(error.message)
-        }
-    }
-
-    private fun changeFocus(digi1: EditText, digi2: EditText, digi3: EditText, digi4: EditText) {
-        enableDigi1(digi1, digi2, digi3, digi4)
-
-        digi1.addTextChangedListener {
-            if (digi1.text.isNotEmpty()){
-                digi2.isEnabled = true
-                digi2.requestFocus()
-            }
-        }
-        digi2.addTextChangedListener {
-            if (digi2.text.isNotEmpty()){
-                digi3.isEnabled = true
-                digi3.requestFocus()
-            }
-        }
-        digi3.addTextChangedListener {
-            if (digi3.text.isNotEmpty()){
-                digi4.isEnabled = true
-                digi4.requestFocus()
-            }
-        }
-    }
-
-    private fun clearFields(digi1: EditText, digi2: EditText, digi3: EditText, digi4: EditText){
-        digi1.text = null
-        digi2.text = null
-        digi3.text = null
-        digi4.text = null
-    }
-
-    private fun enableDigi1(digi1: EditText, digi2: EditText, digi3: EditText, digi4: EditText) {
-        digi1.requestFocus()
-        digi2.isEnabled = false
-        digi3.isEnabled = false
-        digi4.isEnabled = false
-    }
+//    private fun changeFocus(digi1: EditText, digi2: EditText, digi3: EditText, digi4: EditText) {
+//        enableDigi1(digi1, digi2, digi3, digi4)
+//
+//        digi1.addTextChangedListener {
+//            if (digi1.text.isNotEmpty()){
+//                digi2.isEnabled = true
+//                digi2.requestFocus()
+//            }
+//        }
+//        digi2.addTextChangedListener {
+//            if (digi2.text.isNotEmpty()){
+//                digi3.isEnabled = true
+//                digi3.requestFocus()
+//            }
+//        }
+//        digi3.addTextChangedListener {
+//            if (digi3.text.isNotEmpty()){
+//                digi4.isEnabled = true
+//                digi4.requestFocus()
+//            }
+//        }
+//    }
+//
+//    private fun clearFields(digi1: EditText, digi2: EditText, digi3: EditText, digi4: EditText){
+//        digi1.text = null
+//        digi2.text = null
+//        digi3.text = null
+//        digi4.text = null
+//    }
+//
+//    private fun enableDigi1(digi1: EditText, digi2: EditText, digi3: EditText, digi4: EditText) {
+//        digi1.requestFocus()
+//        digi2.isEnabled = false
+//        digi3.isEnabled = false
+//        digi4.isEnabled = false
+//    }
 
 //    override fun onStart() {
 //        super.onStart()
